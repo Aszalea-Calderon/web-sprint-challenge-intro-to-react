ebb2c7befe9b06b5c5066042186048c601781ad0
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-12-11 16:39:53 -0600

    add in new state and useEffect

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DarkBlueBar from "./DarkBlueBar";
 import LargeLights from "./LargeLights";
 import SmallColoredLights from "./SmallColoredLights";
@@ -9,116 +9,185 @@ import InfoDisplay from "./InfoDisplay";
 import Buttons from "./Buttons";
 import axios from "axios";
 
+const initialState = {
+	name: "",
+	height: -1,
+	weight: -1,
+	base: -1,
+	type: "",
+	urlToUse: "",
+}
+
+
+
 const Pokedex = () => {
-  const [search, setSearch] = useState("");
-  const [name, setName] = useState([]);
-  const [height, setHeight] = useState([]);
-  const [weight, setWeight] = useState([]);
-  const [base, setBase] = useState([]);
-  const [type, setType] = useState([]);
-  // const [types, setTypes] = useState([]);
-  // const [photo, setPhoto] = useState[[]];
+	const [name, setName] = useState([]);
+	const [height, setHeight] = useState([]);
+	const [weight, setWeight] = useState([]);
+	const [base, setBase] = useState([]);
+	const [type, setType] = useState([]);
 
-  const change = (event) => {
-    const { value } = event.target;
-    setSearch(value.toLowerCase());
-    // console.log(search);
-  };
+	const [search, setSearch] = useState("pikachu");
+	const [pokemon, setPokemon] = useState(initialState);
+	const [loading, setLoading] = useState(true);
 
-  //onSubmit we are preventing the default reload of the page, setting
-  const submit = (e) => {
-    e.preventDefault();
-    // setDidSearch(true);
-    console.log("submitted!");
+	// const [types, setTypes] = useState([]);
+	// const [photo, setPhoto] = useState[[]];
 
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${search}`)
-      .then((res) => {
-        setName(res.data.forms[0].name);
-        setHeight(res.data.height);
-        setWeight(res.data.weight);
-        setBase(res.data.base_experience);
-        setType(res.data.types[0].type.name);
-        // setPhoto(
-        //   res.data.sprites.versions["generation-v"]["black-white"].animated
-        //     .front_default
-        // );
-      })
-      .catch((err) => {
-        console.error(err, "error");
-      });
-  };
+	const change = (event) => {
+		const { value } = event.target;
+		setSearch(value.toLowerCase());
+		// console.log(search);
+	};
 
-  return (
-    <div>
-      <div className="box">
-        <div className="pokedex">
-          <div className="outer">
-            <DarkBlueBar />
-            <LargeLights />
-            <SmallColoredLights />
+	const hgmToLbs = (hectograms) => {
+		return hectograms / 4.536;
+	}
+	const dmToFt = (decimeters) => {
+		return decimeters / 3.048;
+	}
 
-            <div className="main-border">
-              <div className="display d1"></div>
-              <PhotoDisplay /> {/*photo={photo} />*/}
-              <RandomGenerator />
-              <Gamepad />
-              {/* <!-- end of main-border --> */}
-            </div>
-            <div className="main-border3"></div>
-            <div className="main-border2"></div>
 
-            <div className="cylinder-before"></div>
-            <div className="cylinder"></div>
+	const findUrlToUse = (sprites) => {
+		let urlToUse;
+		if (sprites.versions["generation-v"]["black-white"].animated["front_default"]) {
+			urlToUse = sprites.versions["generation-v"]["black-white"].animated["front_default"];
+		} else {
+			urlToUse = sprites["front_default"];
+		}
+		return urlToUse;
+	}
 
-            {/* <!-- end of outer --> */}
-          </div>
-          <div className="cylinder-after"></div>
-          <div className="flip">
-            <div className="main-border">
-              <InfoDisplay
-                name={name}
-                height={height}
-                weight={weight}
-                base={base}
-                type={type}
-              />
-              <button className="screen"></button>
+	useEffect(() => {
+		if (loading) {
+			axios
+				// .get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+				.get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+				.then((res) => {
+					// const { forms, sprites, height, weight, base_experience, types } = res.data;
+					const { data } = res;
+					setPokemon({
+						...pokemon,
+						name: data.forms[0].name,
+						height: dmToFt(data.height),
+						weight: hgmToLbs(data.weight),
+						base: data["base_experience"],
+						type: data.types[0].type.name,
+						urlToUse: findUrlToUse(data.sprites)
+					});
+				})
+				.catch((err) => {
+					console.error(err, "error");
+				});
+		}
+		setLoading(false);
+	}, [loading]);
 
-              <div>
-                <form onSubmit={submit}>
-                  <input
-                    type="text"
-                    name="searchBar"
-                    value={search.name}
-                    onChange={change}
-                    id="search"
-                    placeholder="Search Pokemon"
-                    className="blue-buttons"
-                  ></input>
-                  <button className="searchButton" type="submit">
-                    Search
+	//onSubmit we are preventing the default reload of the page, setting
+	const submit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+	}
+
+	// const submit = (e) => {
+	// 	e.preventDefault();
+	// 	// setDidSearch(true);
+	// 	console.log("submitted!");
+
+	// 	axios
+	// 		.get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+	// 		.then((res) => {
+	// 			setName(res.data.forms[0].name);
+	// 			setHeight(res.data.height);
+	// 			setWeight(res.data.weight);
+	// 			setBase(res.data.base_experience);
+	// 			setType(res.data.types[0].type.name);
+	// 			// setPhoto(
+	// 			//   res.data.sprites.versions["generation-v"]["black-white"].animated
+	// 			//     .front_default
+	// 			// );
+	// 		})
+	// 		.catch((err) => {
+	// 			console.error(err, "error");
+	// 		});
+	// };
+
+	return (
+		<div>
+			<div className="box">
+				<div className="pokedex">
+
+					<div className="outer">
+						<DarkBlueBar />
+						<LargeLights />
+						<SmallColoredLights />
+
+						<div className="main-border">
+							<div className="display d1"></div>
+							<PhotoDisplay url={pokemon.urlToUse} /> {/*photo={photo} />*/}
+							{/*
+							 <PhotoDisplay url={someState} /> 
+							*/}
+							<RandomGenerator />
+							<Gamepad />
+							{/* <!-- end of main-border --> */}
+						</div>
+						<div className="main-border3"></div>
+						<div className="main-border2"></div>
+
+						<div className="cylinder-before"></div>
+						<div className="cylinder"></div>
+
+						{/* <!-- end of outer --> */}
+					</div>
+					<div className="cylinder-after"></div>
+					<div className="flip">
+						<div className="main-border">
+							<InfoDisplay
+								pokemon={pokemon}
+							// name={name}
+							// height={height}
+							// weight={weight}
+							// base={base}
+							// type={type}
+							/>
+							<button className="screen"></button>
+
+							<div>
+								{/* <form onSubmit={submit}> */}
+								<form onSubmit={submit}>
+									<input
+										type="text"
+										name="searchBar"
+										value={search.name}
+										onChange={change}
+										id="search"
+										placeholder="Search Pokemon"
+										className="blue-buttons"
+									></input>
+									<button className="searchButton" type="submit">
+										Search
                   </button>
-                </form>
-              </div>
-              <Buttons />
+								</form>
+							</div>
+							<Buttons />
 
-              {/* <!-- end of main border --> */}
-            </div>
-            <div className="main-border3"></div>
-            <div className="main-border2"></div>
-            {/* <!-- end of flip --> */}
-            <div className="flip3"></div>
-            <div className="flip2"></div>
-          </div>
+							{/* <!-- end of main border --> */}
+						</div>
+						<div className="main-border3"></div>
+						<div className="main-border2"></div>
+						{/* <!-- end of flip --> */}
+						<div className="flip3"></div>
+						<div className="flip2"></div>
+					</div>
 
-          {/* <!-- end of pokedex --> */}
-        </div>
+					{/* <!-- end of pokedex --> */}
+				</div>
 
-        {/* <!-- end of box --> */}
-      </div>
-    </div>
-  );
+				{/* <!-- end of box --> */}
+			</div>
+		</div>
+	);
 };
 
 export default Pokedex;
